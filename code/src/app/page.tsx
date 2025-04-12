df28116582ad340052a6e3354c427a001039a756
Vivian Cheng <z5332415@ad.unsw.edu.au>
2025-04-13 04:15:27 +1000

    update folder name

---
+++ code/src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+import styles from "./page.module.css";
+import { Box } from "@mui/material";
+
+export default function Home() {
+  // Wrap every letter in a span
+  return (
+    <Box className={styles.page}>
+      hi there
+    </Box>
+  );
+}
